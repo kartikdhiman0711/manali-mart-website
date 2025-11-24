ccdea9f46886b8de7f3521ce3b5ea4fdfc3ff054
anirudhsharma15 <anirudhsharma1505@gmail.com>
2025-11-24 21:21:36 +0530

    connected to remote databse on neon

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,10 +1,5 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
-import 'dotenv/config'
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 
-// Use your .env DATABASE_URL
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-export const db = drizzle(pool);
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql);
